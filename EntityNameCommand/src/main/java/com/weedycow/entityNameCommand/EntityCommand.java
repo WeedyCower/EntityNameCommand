b337cc8874c8beaba661b7d2edca8f7a44f89adc
WeedyCower <1670752910@qq.com>
2023-12-29 17:02:10 +0800

    1.0

--- EntityNameCommand/src/main/java/com/weedycow/entityNameCommand/EntityCommand.java
+++ EntityNameCommand/src/main/java/com/weedycow/entityNameCommand/EntityCommand.java
@@ -1,15 +1,10 @@
 package com.weedycow.entityNameCommand;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
